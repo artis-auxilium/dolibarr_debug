3f6fa46249517fdf73794a4e214b3cbd70c13aec
Dev2a <contact@dev2a.pro>
2026-05-20 23:18:25 +0200

    :sparkles: Ajout option débogage fichier inclus

--- core/modules/syslog/mod_syslog_debug.php
+++ core/modules/syslog/mod_syslog_debug.php
@@ -10,6 +10,11 @@ class mod_syslog_debug extends LogHandler
     public function __construct()
     {
         include_once dirname(__DIR__, 3) . '/lib/debug.lib.php';
+        if (defined('DEBUG_LOADED_FILES')) {
+            register_shutdown_function(function () {
+                debug_log(get_included_files());
+            });
+        }
         $path = dirname(__DIR__, 3) . '/vendor/autoload.php';
         if (!file_exists($path)) {
             $this->active = false;
