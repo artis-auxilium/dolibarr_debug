1f893de7c7feeee6a7a411bcc4584aee91dc04a4
Dev2a <contact@dev2a.pro>
2026-07-23 19:12:54 +0200

    :globe_with_meridians: Prefix traduction

--- class/actions_debug.class.php
+++ class/actions_debug.class.php
@@ -25,7 +25,7 @@ class ActionsDebug
         global $langs;
         $langs->load('debug@debug');
         print '<style>.debug_page_ok {position: fixed; top: 0; left:0;padding: 5px;background-color: var(--colorbackhmenu1);
-                color : var(--colortextbackhmenu);font-size: 0.8rem; width: 20px; height: 20px; overflow: hidden; z-index: 10000} .debug_page_ok:hover { width: 250px }</style><div class="debug_page_ok">'.$langs->trans('EndOFPage').'</div>';
+                color : var(--colortextbackhmenu);font-size: 0.8rem; width: 20px; height: 20px; overflow: hidden; z-index: 10000} .debug_page_ok:hover { width: 250px }</style><div class="debug_page_ok">'.$langs->trans('DebugEndOFPage').'</div>';
         return 0;
     }
 
